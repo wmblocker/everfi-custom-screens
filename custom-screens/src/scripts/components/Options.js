6ffe33b2e646ee0ac57746b7baaf27cd8fbd7a6a
wmblocker <wmblocker@blockertech.com>
2018-02-20 15:47:06 -0500

    Adding Code Box and Editor

--- custom-screens/src/scripts/components/Options.js
+++ custom-screens/src/scripts/components/Options.js
@@ -20,6 +20,41 @@ export class Option1 extends Component{
                         </div>
                     </div>
                 </div>
+                <div className="row">
+                    <div className="col-md-6 code-box">
+                        <pre><code>
+                        &lt;div&gt;<br />
+                            &lt;div className=&quot;cs-modal-1&quot;&gt;<br />
+                                &lt;div className=&quot;header&quot;&gt;<br />
+                                    &lt;p className=&quot;header-text&quot;&gt;Way to go!&lt;/p&gt;<br />
+                                &lt;/div&gt;<br />
+                                &lt;div className=&quot;col-wrap&quot;&gt;<br />
+                                    &lt;div className=&quot;col-25&quot;&gt;<br />
+                                        &lt;div className=&quot;bar&quot;&gt;<br />
+                                        &lt;/div&gt;<br />
+                                    &lt;/div&gt;<br />
+                                    &lt;div className=&quot;col-75&quot;&gt;<br />
+                                        &lt;p className=&quot;sub-heading&quot;&gt;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.&lt;/p&gt;<br />
+                                        &lt;a className=&quot;action-button&quot; href=&quot;#&quot; target=&quot;_blank&quot;&gt;Learn more&lt;/a&gt;<br />
+                                    &lt;/div&gt;<br />
+                                &lt;/div&gt;<br />
+                            &lt;/div&gt;<br />
+                            &lt;div className=&quot;row&quot;&gt;<br />
+                                &lt;div className=&quot;col-md-6 code-box&quot;&gt;<br />
+                                    &lt;pre&gt;&amp;lt;&amp;gt;&lt;/pre&gt;<br />
+                                &lt;/div&gt;<br />
+                                &lt;div className=&quot;col-md-6&quot;&gt;<br />
+                                &lt;/div&gt;<br />
+                            &lt;/div&gt;<br />
+                        &lt;/div&gt;<br />
+            </code>
+                        </pre>
+                    </div>
+                    <div className="col-md-6">
+                    <form>  
+                    </form>
+                    </div>
+                </div>
             </div>
         );
     }
