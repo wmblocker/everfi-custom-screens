ddcb1af36e26c5c42e7cda64cd00f83e8d34cd53
wmblocker <wmblocker@blockertech.com>
2018-02-23 15:04:44 -0500

    Removing <aside> and making links dropdown

--- custom-screens/src/scripts/components/Main.js
+++ custom-screens/src/scripts/components/Main.js
@@ -9,18 +9,22 @@ class Main extends Component{
         return(
             <Switch>
             <div className="row">
-                <aside className="col-md-2 bg-dark" style={{height:"auto"}} >           
-                    <ul>
-                        <li><Link to="/everfi-custom-screens/option1">Option 1</Link></li>
-                        <li><Link to="/everfi-custom-screens/option2">Option 2</Link></li>
-                        <li><Link to="/everfi-custom-screens/option3">Option 3</Link></li>
-                        <li><Link to="/everfi-custom-screens/option4">Option 4</Link></li>
-                        <li><Link to="/everfi-custom-screens/option5">Option 5</Link></li>
-                        <li><Link to="/everfi-custom-screens/option6">Option 6</Link></li>
-                        <li><Link to="/everfi-custom-screens/option7">Option 7</Link></li>
-                    </ul>  
-                </aside> 
-                <main className="col-md-10">
+                <main className="col-md-12">
+                    <div class="dropdown show">
+                        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                           Choose a screen option
+                        </a>
+
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                            <Link className="dropdown-item" to="/everfi-custom-screens/option1">Option 1</Link>
+                            <Link className="dropdown-item" to="/everfi-custom-screens/option2">Option 2</Link>
+                            <Link className="dropdown-item"  to="/everfi-custom-screens/option3">Option 3</Link>
+                            <Link className="dropdown-item"  to="/everfi-custom-screens/option4">Option 4</Link>
+                            <Link className="dropdown-item"  to="/everfi-custom-screens/option5">Option 5</Link>
+                            <Link className="dropdown-item"  to="/everfi-custom-screens/option6">Option 6</Link>
+                            <Link className="dropdown-item"  to="/everfi-custom-screens/option7">Option 7</Link>
+                        </div>
+                    </div>
                     <Route path="/everfi-custom-screens/option1" component={Options.Option1} />
                     <Route path="/everfi-custom-screens/option2" component={Options.Option2} />
                     <Route path="/everfi-custom-screens/option3" component={Options.Option3} />
