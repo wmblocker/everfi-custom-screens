6a346d0ecdbd08956b4234a58199b8869a0c993a
wmblocker <wmblocker@blockertech.com>
2018-03-19 10:59:08 -0700

    Removing References

--- custom-screens/src/scripts/components/NavBar.js
+++ custom-screens/src/scripts/components/NavBar.js
@@ -6,7 +6,7 @@ class NavBar extends Component{
         return( 
             <nav className="navbar navbar-light" style={{marginLeft: "-15px", marginRight:"-15px", backgroundColor: "#464f4e"}}>
                 <a className="navbar-brand" href="#">
-                    <img src={} alt="" style={{height: "50px", maxHeight: "50px"}}/>
+                    <img src="" alt="" style={{height: "50px", maxHeight: "50px"}}/>
                 </a>
             </nav>
         );
