53b2b32825331548fd838f64a6b29eace4f6fe8e
nickwright2471 <nwright@everfi.com>
2018-03-23 14:22:33 -0400

    help icon and submit btn jquery

--- custom-screens/src/scripts/components/Footer.js
+++ custom-screens/src/scripts/components/Footer.js
@@ -5,6 +5,7 @@ class Footer extends Component{
         return( 
             <footer>
                 <nav className="navbar fixed-bottom justify-content-center py-4" style={{backgroundColor: "#393939"}}>
+                	<i className="fa fa-question-circle"></i>
                 </nav>
             </footer>
         );
